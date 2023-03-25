53d10a83cd5b607a4f5dfa9b6fced9125d26a36b
Madhawa Ratnayake <madhawa@intervest.lk>
2023-03-25 16:46:17 +0530

    test complete

--- cypress/e2e/visual-regression-test.cy.js
+++ cypress/e2e/visual-regression-test.cy.js
@@ -4,21 +4,16 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   return false
 })
 
-describe('sampath bank ex', () => {
+describe('Visual Regression Testing', () => {
   beforeEach(() => {
     cy.visit('https://picsum.photos').then(() => {
-      // cy.url().should('eq', 'https://www.sampath.lk/en/exchange-rates')
-      cy.percySnapshot('Sampath Bank');
+      cy.url().should('contain', 'https://picsum.photos')
     })
   })
 
-  // it('displays two todo items by default', () => {
-  //   cy.xpath('//td[contains(text(), "U.K. Pound")]/following-sibling::td[1]').invoke('text').as('tt_buying')
-  //   cy.get('@tt_buying').then((tt_buying) => {
-  //     cy.get('p').contains('Daily Exchange rates as at').invoke('text').then((message) => {
-  //       cy.log(`${message} T/T Buying U.K. Pound ${tt_buying}`)
-  //     })
-  //   })    
-  // })
+  it('look for image difference', () => {
+    cy.title().should('eq', 'Lorem Picsum')
+    cy.percySnapshot('lorem_picsum');
+  })
 
 })
