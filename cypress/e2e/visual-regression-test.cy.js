c8804d9af952875e6a1bd073f5ec49d847e88d91
Madhawa Ratnayake <madhawa@intervest.lk>
2023-03-25 16:06:57 +0530

    first

---
+++ cypress/e2e/visual-regression-test.cy.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false
+})
+
+describe('example to-do app', () => {
+  beforeEach(() => {
+    cy.visit('https://www.sampath.lk/en/exchange-rates').then(() => {
+      cy.url().should('eq', 'https://www.sampath.lk/en/exchange-rates')
+    })
+  })
+
+  it('displays two todo items by default', () => {
+    cy.xpath('//td[contains(text(), "U.K. Pound")]/following-sibling::td[1]').invoke('text').as('tt_buying')
+    cy.get('@tt_buying').then((tt_buying) => {
+      cy.get('p').contains('Daily Exchange rates as at').invoke('text').then((message) => {
+        cy.log(`${message} T/T Buying U.K. Pound ${tt_buying}`)
+      })
+    })
+
+    cy.percySnapshot('Sampath Bank');
+  })
+
+})
